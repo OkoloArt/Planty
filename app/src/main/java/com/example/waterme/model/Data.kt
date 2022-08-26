3e83dc0339ca236ac03fefff481858e8d7a695c4
Arthur Okolo <Okoloarthur4@gmail.com>
2022-08-26 15:37:05 +0100

    Data class and Add Plants

---
+++ app/src/main/java/com/example/waterme/model/Data.kt
@@ -0,0 +1,9 @@
+package com.example.waterme.model
+
+data class PlantData(
+
+    val imageResources : Int,
+    val plantTitle : String,
+    val plantDescription : String,
+    val plantReminder : String
+)
--- app/src/main/java/com/example/waterme/ui/AddPlantFragment.kt
+++ app/src/main/java/com/example/waterme/ui/AddPlantFragment.kt
@@ -1,4 +1,4 @@
-package com.example.waterme
+package com.example.waterme.ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -6,14 +6,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import com.example.waterme.databinding.FragmentSecondBinding
+import com.example.waterme.R
+import com.example.waterme.databinding.FragmentAddPlantBinding
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class SecondFragment : Fragment() {
+class AddPlantFragment : Fragment() {
 
-    private var _binding: FragmentSecondBinding? = null
+    private var _binding: FragmentAddPlantBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -24,7 +25,7 @@ class SecondFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        _binding = FragmentAddPlantBinding.inflate(inflater, container, false)
         return binding.root
 
     }
