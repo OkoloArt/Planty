f9e2ed3bfff9b7efa5743ab93098af58012944ad
Arthur Okolo <Okoloarthur4@gmail.com>
2022-08-30 23:07:30 +0100

    Added write functionality to Database

--- app/src/main/java/com/example/waterme/ui/AddPlantFragment.kt
+++ app/src/main/java/com/example/waterme/ui/AddPlantFragment.kt
@@ -1,13 +1,16 @@
 package com.example.waterme.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import com.example.waterme.R
 import com.example.waterme.databinding.FragmentAddPlantBinding
+import com.example.waterme.model.Plants
+import com.example.waterme.viewmodel.PlantViewModel
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -19,10 +22,11 @@ class AddPlantFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private val plantViewModel: PlantViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View? {
 
         _binding = FragmentAddPlantBinding.inflate(inflater, container, false)
@@ -33,8 +37,19 @@ class AddPlantFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        plantViewModel.result.observe(viewLifecycleOwner) {
+            val message = if (it == null) {
+                getString(R.string.added_plants)
+            } else {
+                getString(R.string.error, it.message)
+            }
+            Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
+        }
         binding.buttonSecond.setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+
+            val plants = Plants( "", "https://cdn.dribbble.com/userupload/3370424/file/original-661f45e4fbb55e46808ff29e152b0641.png?compress=1&resize=752x564",
+                "Herbicus","sibbbii","dqdqdqdqd")
+            plantViewModel.addPlants(plants)
         }
     }
 
