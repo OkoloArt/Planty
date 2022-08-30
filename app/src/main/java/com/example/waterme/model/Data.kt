9d63f1a3c3f33bcf059917a63db66e55847fed72
Arthur Okolo <Okoloarthur4@gmail.com>
2022-08-30 23:04:03 +0100

    ViewModel and Data Class

--- app/src/main/java/com/example/waterme/model/Data.kt
+++ app/src/main/java/com/example/waterme/model/Data.kt
@@ -5,7 +5,6 @@ import com.google.firebase.database.Exclude
 data class Plants(
     @get: Exclude
     var plantId : String? = null,
-
     var imageResources : String? = null,
     var plantTitle : String? = null,
     var plantDescription : String? = null,
--- app/src/main/java/com/example/waterme/viewmodel/PlantViewModel.kt
+++ app/src/main/java/com/example/waterme/viewmodel/PlantViewModel.kt
@@ -5,6 +5,9 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.waterme.NODE_PLANTS
 import com.example.waterme.model.Plants
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 
 class PlantViewModel : ViewModel() {
@@ -14,6 +17,8 @@ class PlantViewModel : ViewModel() {
     private val _result = MutableLiveData<Exception?>()
     val result: LiveData<Exception?> get() = _result
 
+    private val _plants = MutableLiveData<Plants>()
+    val plants: LiveData<Plants> get() = _plants
 
     fun addPlants(plants: Plants) {
         plants.plantId = dbPlants.push().key
@@ -25,5 +30,34 @@ class PlantViewModel : ViewModel() {
                 _result.value = it.exception
             }
         }
+    }
+
+    private val childEventListener = object: ChildEventListener{
+        override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+           val plants = snapshot.getValue(Plants::class.java)
+            plants?.plantId = snapshot.key
+            _plants.value = plants!!
+        }
+
+        override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
+        }
+
+        override fun onChildRemoved(snapshot: DataSnapshot) {
+        }
+
+        override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {
+        }
+
+        override fun onCancelled(error: DatabaseError) {
+        }
+    }
+
+    fun getRealTimeUpdate(){
+        dbPlants.addChildEventListener(childEventListener)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        dbPlants.removeEventListener(childEventListener)
     }
 }
