2786b3d357b701d29fa7950758ffe854185c7a52
Arthur Okolo <Okoloarthur4@gmail.com>
2022-09-11 17:24:57 +0100

    update and delete firebase functionality

--- app/src/main/java/com/example/waterme/viewmodel/PlantViewModel.kt
+++ app/src/main/java/com/example/waterme/viewmodel/PlantViewModel.kt
@@ -9,6 +9,7 @@ import com.google.firebase.database.ChildEventListener
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import java.util.concurrent.Flow
 
 class PlantViewModel : ViewModel() {
 
@@ -19,6 +20,9 @@ class PlantViewModel : ViewModel() {
 
     private val _plants = MutableLiveData<Plants>()
     val plants: LiveData<Plants> get() = _plants
+
+    private val _currentPlant = MutableLiveData<Plants>()
+    val currentPlant: LiveData<Plants> get() = _currentPlant
 
     fun addPlants(plants: Plants) {
         plants.plantId = dbPlants.push().key
@@ -32,17 +36,28 @@ class PlantViewModel : ViewModel() {
         }
     }
 
-    private val childEventListener = object: ChildEventListener{
+    fun setCurrent(plants: Plants?){
+        _currentPlant.value = plants!!
+    }
+
+    private val childEventListener = object : ChildEventListener {
         override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
-           val plants = snapshot.getValue(Plants::class.java)
+            val plants = snapshot.getValue(Plants::class.java)
             plants?.plantId = snapshot.key
             _plants.value = plants!!
         }
 
         override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
+            val plants = snapshot.getValue(Plants::class.java)
+            plants?.plantId = snapshot.key
+            _plants.value = plants!!
         }
 
         override fun onChildRemoved(snapshot: DataSnapshot) {
+            val plants = snapshot.getValue(Plants::class.java)
+            plants?.plantId = snapshot.key
+            plants?.isDeleted = true
+            _plants.value = plants!!
         }
 
         override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {
@@ -52,8 +67,42 @@ class PlantViewModel : ViewModel() {
         }
     }
 
-    fun getRealTimeUpdate(){
+    fun getRealTimeUpdate() {
         dbPlants.addChildEventListener(childEventListener)
+    }
+
+    /**
+     * Retrieve an item from the repository.
+     */
+    fun retrieveItem(plants: Plants): Plants {
+        return plants
+    }
+
+    fun isUserInputValid(plantName: String): Boolean {
+        if (plantName.isBlank()) {
+            return false
+        }
+        return true
+    }
+
+    fun updatePlant(plants: Plants){
+        dbPlants.child(plants.plantId!!).setValue(plants).addOnCompleteListener {
+            if (it.isSuccessful){
+                _result.value=null
+            }else{
+                _result.value=it.exception
+            }
+        }
+    }
+
+    fun deletePlant(plants: Plants){
+        dbPlants.child(plants.plantId!!).setValue(null).addOnCompleteListener {
+            if (it.isSuccessful){
+                _result.value=null
+            }else{
+                _result.value=it.exception
+            }
+        }
     }
 
     override fun onCleared() {
