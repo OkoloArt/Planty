5ca1557ad049a19fd6ca22b5d35c752fc571ac1d
Arthur Okolo <Okoloarthur4@gmail.com>
2022-09-11 17:22:42 +0100

    String file and theme styles

--- app/src/main/java/com/example/waterme/model/Data.kt
+++ app/src/main/java/com/example/waterme/model/Data.kt
@@ -2,11 +2,38 @@ package com.example.waterme.model
 
 import com.google.firebase.database.Exclude
 
+
 data class Plants(
     @get: Exclude
-    var plantId : String? = null,
-    var imageResources : String? = null,
-    var plantTitle : String? = null,
-    var plantDescription : String? = null,
-    var plantReminder : String? = null
-)
+    var plantId: String? = null,
+    var plantImage: String? = null,
+    var plantTitle: String? = null,
+    var plantDescription: String? = null,
+    var plantReminderHour: Int? = null,
+    var plantReminderMinute: Int? = null,
+    var plantAlarmChoice: Boolean? = null,
+    var plantReminderDays: MutableList<String>? = null,
+    val plantAction: MutableList<String>? = null,
+    @get: Exclude
+    var isDeleted: Boolean = false
+    ){
+    override fun equals(other: Any?): Boolean {
+        return if (other is Plants){
+            other.plantId == plantId
+        }else false
+    }
+
+    override fun hashCode(): Int {
+        var result = plantId?.hashCode() ?: 0
+        result = 31 * result + (plantImage?.hashCode() ?: 0)
+        result = 31 * result + (plantTitle?.hashCode() ?: 0)
+        result = 31 * result + (plantDescription?.hashCode() ?: 0)
+        result = 31 * result + (plantReminderHour ?: 0)
+        result = 31 * result + (plantReminderMinute ?: 0)
+        result = 31 * result + (plantAlarmChoice?.hashCode() ?: 0)
+        result = 31 * result + (plantReminderDays?.hashCode() ?: 0)
+        result = 31 * result + (plantAction?.hashCode() ?: 0)
+        result = 31 * result + isDeleted.hashCode()
+        return result
+    }
+}
